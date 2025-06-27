3567c039012ce16321c85962153b1b316ac97452
joji <jorgeborreroc02@gmail.com>
2025-06-27 10:53:19 -0500

    Fix Tailwind app.css

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  cacheDir: 'node_modules/.vite',  // mueve caché afuera de public/build/.vite
   plugins: [
     laravel({
       input: [
@@ -12,20 +10,5 @@ export default defineConfig({
       ],
       refresh: true,
     }),
-    tailwindcss(),
   ],
-  build: {
-    manifest: true,
-    outDir: 'public/build',
-    rollupOptions: {
-      input: [
-        'resources/css/app.css',
-        'resources/js/app.js',
-      ],
-      output: {
-        dir: 'public/build',
-      },
-    },
-  },
-  base: '/',
 });
